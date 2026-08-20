06335541dfeb526866f76bf63bea1e456359ccba
kishore <kishoreb25b@gmail.com>
2026-08-20 18:58:43 +0530

    For and While LOOP

---
+++ src/MiniLab/Loop.java
@@ -0,0 +1,18 @@
+package MiniLab;
+
+public class Loop {
+    public static void main(String[] args) {
+
+        System.out.println("for loop");
+        for (int i=1; i < 6; i++) {
+            System.out.println(i);
+        }
+        System.out.println("while loop");
+        int j=5;
+        while (j>=1){
+            System.out.println(j);
+            j--;
+        }
+    }
+
+}
